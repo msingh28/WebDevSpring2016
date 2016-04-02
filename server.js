3bf9bf7881720c4e13618bef120b75dfd57f3ed3
Monisha Singh <msingh28@ccs.neu.edu>
2016-04-01 23:12:08 -0400

    fixed openshift string

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var multer = require('multer');
 
 var mongoose      = require('mongoose');
 var connectionString = 'mongodb://localhost/FormMakerDB';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
